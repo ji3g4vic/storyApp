a5e16f6a27ea6c3e9f4cdb1d120f1e5581afcb4f
Vic <ji3g4vic03@gmail.com>
2023-01-04 20:59:50 +0800

    Initial Commit

---
+++ storyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  storyApp
+//
+//  Created by 王衍智 on 2023/1/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
